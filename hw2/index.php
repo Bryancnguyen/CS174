c26eacb0840ae019323d15df8c816f495ecfc522
Bryan Nguyen <bryannguyen@Corjee.local>
2017-02-27 14:24:14 -0800

    Fixing Jack's garbage aka relative paths

--- hw2/index.php
+++ hw2/index.php
@@ -25,7 +25,7 @@ function displayIndex(){
 		</head>
 		<body>
 			<h1>Simple Text Editor</h1>
-			<form class="createform" action="/index.php" method="get">
+			<form class="createform" action="./index.php" method="get">
 	 			<input type="hidden" name="file" value="<?= $filename ?>">
 	 			<input type="hidden" name="a" value="create">
 	 			<button class="createButton" type="Submit">Create A New File</button>
@@ -33,21 +33,21 @@ function displayIndex(){
 			<h2>My Files</h2>
 			<table>
 			<tr><th>Filename</th><th colspan="2">Action</th></tr>
-			<?php 
+			<?php
 			$myfiles = getFiles();
-			foreach ($myfiles as $txtFile) { 
+			foreach ($myfiles as $txtFile) {
 				?>
 				 <tr>
 				 	<td><?= $txtFile ?></td>
 				 	<td>
-				 		<form class="indexform" action="/testing/index.php" method="get">
+				 		<form class="indexform" action="./index.php" method="get">
 				 			<input type="hidden" name="file" value="<?= $txtFile ?>">
 				 			<input type="hidden" name="a" value="read">
 				 			<button class="readButton" type="Submit" value="read">Read</button>
 				 		</form>
 				 	</td>
 				 	<td>
-				 		<form class="indexform" action="/index.php" method="get">
+				 		<form class="indexform" action="./index.php" method="get">
 				 			<input type="hidden" name="file" value="<?= $txtFile ?>">
 				 			<input type="hidden" name="a" value="delete">
 				 			<button class="deleteButton" type="Submit" value="delete">Delete</button>
@@ -95,7 +95,7 @@ function confirmDelete($filename){
 		<body>
 			<p>Are you sure you'd like to delete the following file:<b><?= $filename ?></b></p>
 			<!-- Add form to delete the file. -->
-			<form id="deleteForm" action="/index.php?a=delete&file=<?= $filename ?>" method="get">
+			<form id="deleteForm" action="./index.php?a=delete&file=<?= $filename ?>" method="get">
 			</form>
 			<button class="delButton" type="submit" form="deleteForm" value="Submit">Delete</button>
 		</body>
