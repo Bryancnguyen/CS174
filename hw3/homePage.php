5a411a7877c3b62fc0a902beb84ca2bf2c39e4e2
RichardPapalia <richardpapalia@gmail.com>
2017-04-03 15:33:59 -0700

    sample view page

---
+++ hw3/homePage.php
@@ -0,0 +1,27 @@
+<h1 style="font-size:20pt;"><a href=".">Note-A-List</a></h1>
+<?php
+
+function render($categories, $notes){
+  
+  echo '<h2 style="margin-bottom:1px;padding-bottom:1px;">Lists</h2>';
+  echo '<ul>';
+  foreach($categories as $cat){
+    echo '<li>'.$cat.'</li>';
+  }
+
+  echo '</ul>';
+  echo '<h2 style="margin-bottom:1px;padding-bottom:1px;">Notes</h2>';
+
+  echo '<ul>';
+  foreach($notes as $note){
+    echo '<li>'.$note.'</li>';
+  }
+
+  echo '</ul>';
+
+}
+
+$a = [1,2,3,4];
+$b = [2,3,4,5];
+render($a, $b);
+?>
