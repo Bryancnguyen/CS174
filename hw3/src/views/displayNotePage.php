9a6d3441a428968f4a02c7fd474b189eebef6802
Richard Papalia <richardpapalia@gmail.com>
2017-04-04 01:27:45 -0700

    Add files via upload

---
+++ hw3/src/views/displayNotePage.php
@@ -0,0 +1,15 @@
+
+
+<?php
+
+function render($categories){
+
+  echo '<h1 style="font-size:20pt;"><a href=".">Note-A-List/PHP that will determine what sublist</a></h1>';
+  echo '<h2 style="margin-bottom:1px;padding-bottom:1px;">Note:/Magical PHP that will determine which note</h2>';
+  echo '<p>More magical php that will tell the notes description';
+
+
+}
+
+render($a)
+?>
