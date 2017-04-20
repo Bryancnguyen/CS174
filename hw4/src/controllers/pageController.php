b4187bed7699596d4f70ddde18a36c68282acd8b
RichardPapalia <richardpapalia@gmail.com>
2017-04-20 13:32:40 -0700

    commenting what i need to do

--- hw4/src/controllers/pageController.php
+++ hw4/src/controllers/pageController.php
@@ -10,12 +10,18 @@ use \cs174\hw4\views as V;
 class PageController {
     private $data;
     function render() {
-    $data = '';
-    $this->landingView = new V\landingPage('WebLayout');
-    $this->landingView->display($data);
-    // $this->editSheetView = new V\editSheetPage('WebLayout');
-    // $this->editSheetView->display($data);
-    // $this->readSheetView = new V\readSheetPage('WebLayout');
-    // $this->readSheetView->display($data);
+    $data = ''; //Create An Instance of the data model
+
+    //Check if the posted value from landingPage.php is already in the data model, if it is pass that object to viewHelper
+
+    //If it is not then create that object and pass it to the view.
+
+
+    // $this->landingView = new V\landingPage('WebLayout');
+    // $this->landingView->display($data);
+    // // $this->editSheetView = new V\editSheetPage('WebLayout');
+    // // $this->editSheetView->display($data);
+    // // $this->readSheetView = new V\readSheetPage('WebLayout');
+    // // $this->readSheetView->display($data);
     }
 }
