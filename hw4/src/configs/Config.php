266acf798b50e2c16150fd4856248699851687d9
RichardPapalia <richardpapalia@gmail.com>
2017-04-21 01:06:51 -0700

    installed composer and updated controller

--- hw4/src/configs/Config.php
+++ hw4/src/configs/Config.php
@@ -2,5 +2,5 @@
 namespace cs174\hw4\configs;
 
 const DB_USR = "root";
-const DB_PWD = "cs160";
+const DB_PWD = "joseph";
 const DB_PRT = "3306";
--- hw4/src/controllers/pageController.php
+++ hw4/src/controllers/pageController.php
@@ -8,29 +8,40 @@ namespace cs174\hw4\controllers;
 use \cs174\hw4\views as V;
 
 class PageController {
-    private $data;
-    private $landingView;
+  private $data;
+  private $sheetData;
+  private $landingView;
+
     function render() {
 
     if(isset($_POST['userInput'])) //Check if the user actually put any input
     {
       $inputString = filter_var($_POST['userInput'], FILTER_SANITIZE_STRING); // Get the users input in a string
       $this->data = new \cs174\hw4\models\Sheet($inputString); //Create a sheet with the users input name
+      $this->sheetData = new \cs174\hw4\models\Sheet_Code($inputString); //create a sheet_code with users input.. may or may not exist
 
       if($data->valid){ //If this sheet is already in the database
         $this->editSheetView = new V\editSheetPage('WebLayout');//Create the view
         $this->editSheetView->display($data);//Pass the data to View
       }
+
+      if($sheetData->valid){
+        $sheetCodeName = $sheetData->sheet_name; //Get the name of the sheet which corresponds to the sheet_data
+        $dataToPass = new \cs174\hw4\models\Sheet($sheetCodeName); //create a sheet object to pass to view
+        $this->editSheetView = new V\editSheetPage('WebLayout');//Create the view
+        $this->editSheetView->display($data);//Pass the data to View
+      }
+
       else{
-        $sheet_to_pass = new \cs174\hw4\models\Sheet("sheet_one", '{[ ["Tom", "Sally"] ]}');
+        $sheet_to_pass = new \cs174\hw4\models\Sheet($inputString, '{[ ["Tom", "Sally"] ]}');
         $this->editSheetView = new V\editSheetPage('WebLayout');//Create the view
         $this->editSheetView->display($data);//Pass the data to View
       }
     }
-    else{
+    else{ //There was no user input pass an em
       $data = '';
-      $this->editSheetView = new V\editSheetPage('WebLayout');//Create the view
-      $this->editSheetView->display($data);//Pass the data to View
+      $this->landingView = new V\landingPage('WebLayout');//Create the view
+      $this->landingView->display($data);//Pass the data to View
     }
 
 
