48cb45694e529598fbe0a9e049aafb438153ad68
jtwanke <jtwanke@comast.net>
2017-04-24 17:00:07 -0700

    Added json post parsing.

--- hw4/src/controllers/ApiController.php
+++ hw4/src/controllers/ApiController.php
@@ -6,13 +6,27 @@ use \cs174\hw4\models as M;
 
 class ApiController {
 
-	public function __construct($model, $data) {
-		$this->updateModel($model, $data);
+	public function __construct($model) {
+		$this->updateModel($model);
 	}
 
-	private function updateModel($model, $data){
-		$sheet = new M\Sheet($model);
-		$this->respond($sheet->update($data));
+	private function updateModel($model){
+		$contentType = isset($_SERVER["CONTENT_TYPE"]) ? trim($_SERVER["CONTENT_TYPE"]) : '';
+		if(strcasecmp($contentType, 'application/json') == 0){
+			$content = trim(file_get_contents("php://input"));
+			$decoded = json_decode($content, true);
+		    if(is_array($decoded)){
+		    	$sheet = new M\Sheet($model);
+				$this->respond($sheet->update($content));
+			}
+			else{
+				print("Wrong json encoding: $content");
+			}
+			
+		}
+		else{
+			print("Wrong encoding header.");
+		}
 	}
 
 	private function respond($status){
